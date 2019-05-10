b1163d892142e9377f5267e3ead92e5cf989876f
anhyunji <31725513+anhyunji@users.noreply.github.com>
2019-05-10 11:23:35 +0900

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include<iostream>
+#include<iomanip>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int arr[100010]; ////////여유있게 10더해서 저장해줌
+int n, i, j;
+
+void insertion_sort();
+
+int main() {
+
+	srand((unsigned)time(NULL));
+	for (i = 0; i < 100000; i++) {
+		arr[i] = rand();                  //////렌덤으로 10만개 난수 생성
+
+	}
+
+	cout << "array Before  InsertionSort" << "\n";
+	for (i = 0; i < 100000; i++)
+		cout << setw(2) << arr[i] << " ";            /////////////정렬되기 전 데이터 10만개 출력
+	cout << "\n";
+
+
+	insertion_sort(); //do InsertionSort 
+
+	cout << "array After InsertionSort" << "\n";
+	for (i = 0; i < 100000; i++)
+		cout << setw(2) << arr[i] << " ";             /////////////정렬 후 데이터 10만개 출력
+	cout << "\n";
+
+	return 0;
+}
+
+void insertion_sort() {
+	int key;
+
+	for (i = 1; i < 100000; i++) {
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && key < arr[j]) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
